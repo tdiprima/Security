d98d473a3708444f47c9440bb51ccac2cbf631a4
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-09-26 10:26:27 -0400

    1 hour

--- src/main/java/org/example/JwtTokenUtil.java
+++ src/main/java/org/example/JwtTokenUtil.java
@@ -21,7 +21,7 @@ public class JwtTokenUtil {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hour expiration
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
